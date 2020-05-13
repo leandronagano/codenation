f74d3e2ec65588f7c31d47f9f19e6da5d24fc871
Leandro Nagano <leandronagano@gmail.com>
2020-05-12 22:48:52 -0300

    aula2

---
+++ Modulo 1/tipos_builtin.py
@@ -0,0 +1,15 @@
+lista_familia = ['Leandro', 'Paula', 'Felipe', 'Manuela']
+
+print(lista_familia)
+
+lista_familia.append('Maggie')
+
+print(lista_familia)
+
+lista_familia.remove('Maggie')
+
+print(lista_familia)
+
+print(lista_familia[0])
+
+print(lista_familia[-2])
