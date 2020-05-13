5f79d375186d924f655cc046c7fee4b9edf25cf3
Leandro Nagano <leandronagano@gmail.com>
2020-05-12 22:59:55 -0300

    aula 3

--- Modulo 1/tipos_builtin.py
+++ Modulo 1/tipos_builtin.py
@@ -13,3 +13,26 @@ print(lista_familia)
 print(lista_familia[0])
 
 print(lista_familia[-2])
+
+#tuple
+
+tupla1 = (1, 2, 3)
+tupla2 = (4, 5, 6)
+
+tupla3 = tupla1 + tupla2
+
+print(tupla3)
+
+# chave -> valor
+
+dados_pessoais = {'nome': 'Batman', 'cidade': 'Gothan'}
+
+print(dados_pessoais)
+
+dados_pessoais['veiculo'] = 'Batmovel'
+
+print(dados_pessoais)
+
+del dados_pessoais['cidade']
+
+print(dados_pessoais)
