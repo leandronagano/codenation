d8ef7a51bf9764a79363fd6c9a5622e29085c1f4
Leandro Nagano <leandronagano@gmail.com>
2020-05-12 22:40:21 -0300

    aula1

---
+++ Modulo 1/variaveis_primitivas.py
@@ -0,0 +1,21 @@
+nome = 'Leandro Nagano'
+idade = 35
+altura = 1.73
+aluno = True
+trabalhando = None
+
+print(nome)
+print(idade)
+print(altura)
+print(aluno)
+print(trabalhando)
+
+nova_idade = idade + 30
+nome_cidade = nome + ' Campinas'
+
+print(nova_idade)
+print(nome_cidade)
+
+altura_sem_tenis = altura - 0.05
+
+print(altura_sem_tenis)
